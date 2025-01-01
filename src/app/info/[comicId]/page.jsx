214d72de5d2d4a3b1786eceec410e8541715d7a9
Chris Williams <cchriswilliams721@gmail.com>
2025-01-01 11:51:26 -0700

    Fixed info alignment

--- src/app/info/[comicId]/page.jsx
+++ src/app/info/[comicId]/page.jsx
@@ -47,23 +47,27 @@ export default function Info() {
   if (error) return <p>Error: {error}</p>;
 
   return (
-    <div className="max-w-3xl mx-auto p-4">
-      <h1 className="text-3xl font-bold text-white mb-4">{comic.title}</h1>
+    <div className="max-w-4xl mx-auto p-4">
+      <h1 className=" text-3xl font-bold text-white mb-4">{comic.title}</h1>
+      <div className='flex space-x-4'>
       {comic.thumbnail && (
         <img
           src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`}
           alt={comic.title}
-          className="w-full rounded-lg mb-4"
+          className=" rounded-lg mb-4 size-[500px] "
         />
       )}
+      <div className='flex flex-col'>
       <p className="text-white text-lg">{comic.description || "No description available."}</p>
       <p className="text-gray-400 mt-4">Page Count: {comic.pageCount}</p>
-      <p className="text-gray-400 mt-1">
+      <p className="text-gray-400 mt-4">
         Published: {new Date(comic.dates.find((d) => d.type === "onsaleDate")?.date).toLocaleDateString()}
       </p>
       <p className="text-gray-400 mt-1">
         Price: ${comic.prices.find((p) => p.type === "printPrice")?.price || "N/A"}
       </p>
+      </div>
+      </div>
     </div>
   );
 }
