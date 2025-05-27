3a3eb41b44c3c72742b3ec1dd8193a06fbea92ae
Chris Williams <cchriswilliams721@gmail.com>
2025-05-27 10:58:30 -0600

    stashed changes

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Nav from '../components/navbar'
 import Popup from '../components/modal'
-
+//Add all user blogs on here
 export default function Blog() {
   return (
     <div>
         <Nav/>
-        <Popup/>
+        
     </div>
   )
 }
